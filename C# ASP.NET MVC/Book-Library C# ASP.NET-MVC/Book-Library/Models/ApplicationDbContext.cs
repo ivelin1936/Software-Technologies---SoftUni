7be31e588fc6ec42bccb521439d263902a12e21c
Ivelin1936 <ivelin_1936@abv.bg>
2017-12-12 16:26:51 +0200

    add Book-Library C# ASP.NET-MVC and update Calculator C# ASP.NET-MVC

---
+++ C# ASP.NET MVC/Book-Library C# ASP.NET-MVC/Book-Library/Models/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.Entity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Book_Library.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
