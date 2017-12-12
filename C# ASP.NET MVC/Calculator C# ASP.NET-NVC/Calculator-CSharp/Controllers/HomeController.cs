9ffbe515925a7eae09252b17dddcd344eb650f3e
Ivelin1936 <ivelin_1936@abv.bg>
2017-12-12 13:33:17 +0200

    add Calculator C# ASP.NET-NVC

---
+++ C# ASP.NET MVC/Calculator C# ASP.NET-NVC/Calculator-CSharp/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Web.Mvc;
+using Calculator_CSharp.Models;
+
+namespace Calculator_CSharp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(Calculator calculator)
+        {
+            return View(calculator);
+        }
+
+        [HttpPost]
+        public ActionResult Calculate(Calculator calculator)
+        {
+            calculator.Result = CalculateResult(calculator);
+
+            return RedirectToAction("Index", calculator);
+        }
+
+        private decimal CalculateResult(Calculator calculator)
+        {
+            var result = 0m;
+
+            switch (calculator.Operator)
+            {
+                case "+":
+                    result = calculator.LeftOperand + calculator.RightOperand;
+                    break;
+                case "-":
+                    result = calculator.LeftOperand - calculator.RightOperand;
+                    break;
+                case "*":
+                    result = calculator.LeftOperand * calculator.RightOperand;
+                    break;
+                case "/":
+                    result = calculator.LeftOperand / calculator.RightOperand;
+                    break;
+                case "mod %":
+                    result = calculator.LeftOperand % calculator.RightOperand;
+                    break;
+                case "pow":
+                    decimal buffRresult = 1;
+                    for (int i = 1; i <= calculator.RightOperand; i++)
+                    {
+                        buffRresult = buffRresult * calculator.LeftOperand;
+                    }
+                    result = buffRresult;
+                    break;
+            }
+            return result;
+        }
+    }
+}
