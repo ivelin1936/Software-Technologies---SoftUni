da768864c947d06bcc64122de946355200a3ddc4
Ivelin1936 <ivelin_1936@abv.bg>
2017-12-14 13:16:34 +0200

    add FilmsCatalogNoteBook C# ASP.Net MVC

---
+++ C# ASP.NET MVC/FilmsCatalogNoteBook C# ASP/IMDB/Models/Film.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace IMDB.Models
+{
+    public class Film
+    {
+        [Key] //With and without it will work
+        public int Id { get; set; }
+
+        [Required] //-> Not null 
+        [MinLength(1)] //-> non-empty text
+        public string Name { get; set; }
+
+        [Required]
+        [MinLength(1)]
+        public string Genre { get; set; }
+
+        [Required]
+        [MinLength(1)]
+        public string Director { get; set; }
+
+        [Required]
+        [Range(1900, 2100)]
+        public int Year { get; set; }
+    }
+}
