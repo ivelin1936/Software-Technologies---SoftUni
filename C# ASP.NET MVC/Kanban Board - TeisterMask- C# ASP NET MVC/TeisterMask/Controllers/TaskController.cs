1fe123f6dbda1f6dd90fb84c9c415ad82fba3761
Ivelin1936 <ivelin_1936@abv.bg>
2017-12-16 20:46:08 +0200

    add Kanban Board - TeisterMask- C# ASP.NET MVC

---
+++ C# ASP.NET MVC/Kanban Board - TeisterMask- C# ASP NET MVC/TeisterMask/Controllers/TaskController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using TeisterMask.Models;
+
+namespace TeisterMask.Controllers
+{
+    [ValidateInput(false)]
+    public class TaskController : Controller
+    {
+
+        [HttpGet]
+        [Route("")]
+        public ActionResult Index()
+        {
+            using (var context = new TeisterMaskDbContext())
+            {
+                List<Task> tasks = context.Tasks.ToList();
+
+                return View(tasks);
+            }
+        }
+
+        [HttpGet]
+        [Route("create")]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("create")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Task task)
+        {
+            using (var context = new TeisterMaskDbContext())
+            {
+                context.Tasks.Add(task);
+                context.SaveChanges();
+
+                return this.Redirect("/");
+            }
+        }
+
+        [HttpGet]
+        [Route("edit/{id}")]
+        public ActionResult Edit(int id)
+        {
+            using (var context = new TeisterMaskDbContext())
+            {
+                var taskFromDb = context.Tasks.Find(id);
+                if (id == null)
+                {
+                    return HttpNotFound();
+                }
+
+                return View(taskFromDb);
+            }
+        }
+
+        [HttpPost]
+        [Route("edit/{id}")]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditConfirm(int id, Task taskModel)
+        {
+            using (var context = new TeisterMaskDbContext())
+            {
+                var taskFromDb = context.Tasks.Find(id);
+                if (id == null)
+                {
+                    return HttpNotFound();
+                }
+
+                if (this.ModelState.IsValid)
+                {
+                    taskFromDb.Title = taskModel.Title;
+                    taskFromDb.Status = taskModel.Status;
+
+                    context.SaveChanges();
+
+                    return Redirect("/");
+                }
+
+                return View("Edit", taskModel);
+            }
+        }
+    }
+}
---
+++ C# ASP.NET MVC/Kanban Board - TeisterMask- C# ASP NET MVC/TeisterMask/Models/Task.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TeisterMask.Models
+{
+    public class Task
+    {
+        [Key] //With and without it will work
+        public int Id { get; set; }
+
+        [Required] //-> Not null 
+        [MinLength(1)] //-> non-empty text
+        public string Title { get; set; }
+
+        [Required]
+        [MinLength(1)]
+        public string Status { get; set; }
+    }
+}
